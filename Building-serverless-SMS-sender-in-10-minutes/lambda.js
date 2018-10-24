764f1293bc0d9ac954509e8c7b51f899ac548468
KMKUMAR225 <kommanamadhukumar@gmail.com>
2018-10-24 13:27:18 +0530

    success

---
+++ Building-serverless-SMS-sender-in-10-minutes/lambda.js
@@ -0,0 +1,38 @@
+let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+
+exports.handler = function (event, context, callback) {
+
+    let receiver = event['receiver'];
+    let sender = event['sender'];
+    let message = event['message'];
+
+    console.log("sending message", message, "to receiver", receiver);
+    sns.publish({
+        Message: 'hi,how are you',
+        MessageAttributes: {
+            'AWS.SNS.SMS.SMSType': {
+                DataType: 'String',
+                StringValue: 'Promotional'
+            },
+            'AWS.SNS.SMS.SenderID': {
+                DataType: 'String',
+                StringValue: 'US'
+            }
+        },
+        PhoneNumber: '+91728816344'
+    }).promise()
+        .then(data => {
+            // your code goes here
+            console.log("sent message to", receiver);
+            callback(null, data);
+
+        })
+        .catch(err => {
+            // error handling goes here
+            console.log("sending failed", err);
+            callback(err);
+        });
+
+    callback(null, 'Successfully executed');
+}
